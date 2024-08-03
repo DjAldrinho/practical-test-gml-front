98a4daafe583c5481434f9cbd5974cdecd6bdece
Aldray Narvaez <aldray.devsoft@gmail.com>
2024-08-03 13:03:08 -0500

    Install firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,7 +4,14 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideHttpClient } from '@angular/common/http';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { initializeAppCheck, ReCaptchaEnterpriseProvider, provideAppCheck } from '@angular/fire/app-check';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(), provideHttpClient()]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(), provideHttpClient(), provideFirebaseApp(() => initializeApp({"projectId":"practical-test-gml","appId":"1:745535521305:web:90df3c8ba16132c3ffa088","storageBucket":"practical-test-gml.appspot.com","apiKey":"AIzaSyDmF0ujG_7h6SKZMKAbvSiQOtVs1OBjAS4","authDomain":"practical-test-gml.firebaseapp.com","messagingSenderId":"745535521305"})), provideAppCheck(() => {
+  // TODO get a reCAPTCHA Enterprise here https://console.cloud.google.com/security/recaptcha?project=_
+  const provider = new ReCaptchaEnterpriseProvider(/* reCAPTCHA Enterprise site key */);
+  return initializeAppCheck(undefined, { provider, isTokenAutoRefreshEnabled: true });
+}), provideFirestore(() => getFirestore())]
 };
