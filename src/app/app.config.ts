c109fc3d5d03ed2c78c4fb67a75ed144618ca373
Aldray Narvaez <aldray.devsoft@gmail.com>
2024-08-03 13:30:00 -0500

    Add Netlify

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,13 +4,6 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideHttpClient } from '@angular/common/http';
-import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
-import {
-  initializeAppCheck,
-  ReCaptchaEnterpriseProvider,
-  provideAppCheck,
-} from '@angular/fire/app-check';
-import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -18,27 +11,5 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideClientHydration(),
     provideHttpClient(),
-    provideFirebaseApp(() =>
-      initializeApp({
-        projectId: 'practical-test-gml',
-        appId: '1:745535521305:web:90df3c8ba16132c3ffa088',
-        storageBucket: 'practical-test-gml.appspot.com',
-        apiKey: 'AIzaSyDmF0ujG_7h6SKZMKAbvSiQOtVs1OBjAS4',
-        authDomain: 'practical-test-gml.firebaseapp.com',
-        messagingSenderId: '745535521305',
-      })
-    ),
-    provideFirestore(() => getFirestore()),
-    provideFirebaseApp(() =>
-      initializeApp({
-        projectId: 'practical-test-gml',
-        appId: '1:745535521305:web:90df3c8ba16132c3ffa088',
-        storageBucket: 'practical-test-gml.appspot.com',
-        apiKey: 'AIzaSyDmF0ujG_7h6SKZMKAbvSiQOtVs1OBjAS4',
-        authDomain: 'practical-test-gml.firebaseapp.com',
-        messagingSenderId: '745535521305',
-      })
-    ),
-    provideFirestore(() => getFirestore()),
   ],
 };
